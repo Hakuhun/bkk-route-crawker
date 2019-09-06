a5be9859a1a392b70574e6ba767b9891182e99ca
Bakonyi Gergő <bakonyi.gergo.istvanv@gmail.com>
2019-09-06 07:32:29 +0200

    Project creation

---
+++ src/main/java/hu/oe/bakonyi/bkk/bkkroutecrawler/BkkroutecrawlerApplication.java
@@ -0,0 +1,13 @@
+package hu.oe.bakonyi.bkk.bkkroutecrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkkroutecrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkkroutecrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/oe/bakonyi/bkk/bkkroutecrawler/BkkroutecrawlerApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.oe.bakonyi.bkk.bkkroutecrawler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BkkroutecrawlerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
