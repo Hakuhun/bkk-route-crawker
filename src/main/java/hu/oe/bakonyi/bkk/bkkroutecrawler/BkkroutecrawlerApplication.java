32b6bccd93b17539d26c1f03d91fe0f14533fc07
Bakonyi Gergő <bakonyi_gergo@4dsoft.hu>
2019-12-07 04:11:44 +0100

    Dependency cleanup

--- src/main/java/hu/oe/bakonyi/bkk/bkkroutecrawler/BkkroutecrawlerApplication.java
+++ src/main/java/hu/oe/bakonyi/bkk/bkkroutecrawler/BkkroutecrawlerApplication.java
@@ -1,19 +1,13 @@
 package hu.oe.bakonyi.bkk.bkkroutecrawler;
 
-import feign.Logger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication()
 @EnableFeignClients
-@EnableKafka
 @EnableScheduling
 @ComponentScan("hu.oe.bakonyi")
 public class BkkroutecrawlerApplication {
